6a8ffa926472d51a37691c335169273ef18f1b68
dmitrymitenkoff <d.mitenkoff@icloud.com>
2021-10-27 10:47:57 +1300

    Updated the toggle menu functionality

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 const button = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.menu-list');
+const menu = document.querySelector('#menu-list');
+const dropDownButtons = document.querySelectorAll('.dropdown__title');
+const carets = document.querySelectorAll('.caret');
+
+menu.classList.add('hidden');
 
 // Toggle menu open/close
 function toggleMenu(status) {
   if (status === 'false') {
-    menu.removeAttribute('hidden');
+    menu.classList.remove('hidden');
+    menu.classList.add('grid');
     button.setAttribute('aria-expanded', 'true');
     button.setAttribute('aria-label', 'Close the menu');
   } else {
-    menu.setAttribute('hidden', 'hidden');
+    menu.classList.add('hidden');
+    menu.classList.remove('grid');
     button.setAttribute('aria-expanded', 'false');
     button.setAttribute('aria-label', 'Open the menu');
   }
@@ -17,4 +23,15 @@ function toggleMenu(status) {
 button.addEventListener('click', event => {
   const status = button.getAttribute('aria-expanded');
   toggleMenu(status);
+});
+
+dropDownButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const expanded = btn.getAttribute('aria-expanded');
+    btn.setAttribute('aria-expanded', !expanded);
+    const c = btn.firstElementChild;
+    c.classList.toggle('close');
+    const subMenu = btn.nextElementSibling;
+    subMenu.hidden = !subMenu.hidden;
+  });
 });
